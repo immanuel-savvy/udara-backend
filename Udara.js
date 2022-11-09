e0d63f8e626b5ac964c6943cd92501eee00cc5e8
“immanuel-savvy” <immanuelsavvy@gmail.com>
2022-11-09 14:16:18 +0100

    1667999778579267169

--- Udara.js
+++ Udara.js
@@ -27,8 +27,8 @@ const paga_collection_client = new PagaCollectClient()
   .build();
 
 app.use(express.static(__dirname + "/Assets"));
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false, limit: "100mb" }));
+app.use(bodyParser.json({ limit: "100mb" }));
 
 app.get("/", (req, res) =>
   res.send(
